60373223ecf2928726a506bef97aa9803d788d5c
ozodsilver <ozodsilver@gmail.com>
2023-12-14 20:45:39 +0500

    home navigaton added

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,44 +1,57 @@
 <template>
-  <div class="min-h-[700px] bg-black wrapper relative  overflow-hidden">
+  <div class="min-h-[700px] bg-black wrapper relative overflow-hidden">
     <div class="fixed top-0 z-50 w-full">
       <Navigation></Navigation>
     </div>
 
-    <Carousel :autoplay="40000" :itemsToShow = '1' :wrap-around="true" >
+    <Carousel :autoplay="40000" :itemsToShow="1" :wrap-around="true">
       <Slide v-for="image in images" :key="image">
-        <div class="carousel__item p-0 relative top-[-10px] "> 
-          <img :src="image.image_url" class="object-cover w-full brightness-75 h-[750px]" alt="">
-      <div class="absolute hover:scale-110 hover:z-[99999] transition w-[350px] top-[160px] left-[100px] md:w-[400px] bg_description  p-5 md:p-10 rounded-lg  text-white font-bold  quicksand-700 ">
-        <h1 class="text-start text-3xl uppercase first-letter:font-extrabold  first-letter:text-5xl first-letter:text-teal-500  "> {{ image.content[0].title }}</h1>
-    <p class="text-xs md:text-base text-start relative mt-2  ">   {{ image.content[0].text }}</p>
+        <div class="carousel__item p-0 relative top-[-10px]">
+          <img
+            :src="image.image_url"
+            class="object-cover w-full brightness-75 h-[750px]"
+            alt=""
+          />
 
-      </div>
-        
+
+          <div
+            class="absolute hover:scale-110 hover:z-[99999] transition w-[280px] md:top-[160px] md:left-[100px]  left-[30px] top-[140px] md:w-[400px] bg_description p-5 md:p-10 rounded-lg text-white font-bold quicksand-700 overflow-hidden"
+          >
+            <h1
+              class="text-start md:text-3xl uppercase first-letter:font-extrabold first-letter:text-5xl first-letter:text-red-500"
+            >
+              {{ image.content[0].title }}
+            </h1>
+            <p class="text-xs md:text-base text-start relative mt-2">
+              {{ image.content[0].text }}
+            </p>
+          </div>
+
+
         </div>
 
-
-
-        <div class=" p-0  top-[-20px]  "> 
-          
-      <div class="absolute hover:scale-110 transition w-[350px] left-[200px] top-[260px]   md:w-[400px] bg_description  p-5 md:p-10 rounded-lg  text-white font-bold  quicksand-700 ">
-        <h1 class="text-start text-3xl uppercase first-letter:font-extrabold  first-letter:text-5xl first-letter:text-teal-500  ">{{ image.content[1].title }}</h1>
-    <p class="text-xs md:text-base text-start relative mt-2 s ">   {{ image.content[1].text }}</p>
-
-      </div>
-
-     
-        
+        <div class="p-0 top-[-20px]">
+          <div
+            class="absolute hover:scale-110 transition w-[280px] md:left-[200px] md:top-[260px] top-[170px] left-[60px] md:w-[400px] bg_description p-5 md:p-10 rounded-lg text-white font-bold quicksand-700 overflow-hidden"
+          >
+            <h1
+              class="text-start md:text-3xl uppercase first-letter:font-extrabold first-letter:text-5xl first-letter:text-teal-500"
+            >
+              {{ image.content[1].title }}
+            </h1>
+            <p class="text-xs md:text-base text-start relative mt-2 s">
+              {{ image.content[1].text }}
+            </p>
+          </div>
         </div>
 
-
-
-
-        <div class="w-[400px] h-[400px]  absolute right-[150px] flex items-center" >
-          <h1 class="text-white font-semibold text-2xl text-justify ">{{ image.main_title }}</h1>
+        <div
+          class="md:w-[400px] h-[400px] absolute md:right-[150px] flex items-center "
+        >
+          <h1 class="text-white font-semibold md:text-2xl px-5 md:px-0 md:text-justify text-center">
+            {{ image.main_title }}
+          </h1>
         </div>
-
-
-       
       </Slide>
     </Carousel>
   </div>
@@ -50,42 +63,40 @@ import TabPanel from "primevue/tabpanel";
 import { Carousel, Pagination, Slide } from "vue3-carousel";
 import "vue3-carousel/dist/carousel.css";
 
-
 import Navigation from "../Navigation/Navigation.vue";
 
-
 const images = [
-    {
-      "image_url" : "https://w.forfun.com/fetch/1f/1f1ffcabbcce195aa0b1364a57bd8ad5.jpeg",
-      "main_title" : "Eng yaxshi sarflangan mablag' bu ilmga sarflangan mablag'dir",  
-      "content" : [
-          {
-            "title" : "HeaderNew1Lorem1", 
-            "text" : "HeaderNews1LoremText1"
-          }, 
-          {
-            "title" : "HeaderNews1Lorem2", 
-            "text" : "HeaderNews1LoremText2"
-          
-          }
-        ]
-    }, 
-    {
-      "image_url" : "https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_1280.jpg", 
-      "main_title" : "ilm ilm", 
-      "content" : [
-          {
-            "title" : "HeaderNew2Lorem3", 
-            "text" : "HeaderNews2LoremText3"
-          }, 
-          {
-            "title" : "HeaderNews2Lorem4", 
-            "text" : "HeaderNews2oremText4"
-          
-          }
-        ]
-    }
-]; 
+  {
+    image_url:
+      "https://w.forfun.com/fetch/1f/1f1ffcabbcce195aa0b1364a57bd8ad5.jpeg",
+    main_title: "Eng yaxshi sarflangan mablag' bu ilmga sarflangan mablag'dir",
+    content: [
+      {
+        title: "HeaderNew1Lorem1",
+        text: "HeaderNews1LoremText1",
+      },
+      {
+        title: "HeaderNews1Lorem2",
+        text: "HeaderNews1LoremText2 lore lore lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem",
+      },
+    ],
+  },
+  {
+    image_url:
+      "https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_1280.jpg",
+    main_title: "ilm ilm",
+    content: [
+      {
+        title: "HeaderNew2Lorem3",
+        text: "HeaderNews2LoremText3",
+      },
+      {
+        title: "HeaderNews2Lorem4",
+        text: "HeaderNews2oremText4",
+      },
+    ],
+  },
+];
 </script>
 
 <style lang="scss" scoped>
@@ -133,20 +144,16 @@ const images = [
   border: 5px solid white;
 }
 
-
-.quicksand-700{
-  font-family: 'Quicksand', sans-serif;
+.quicksand-700 {
+  font-family: "Quicksand", sans-serif;
 }
 
-.bg_description{
-  backdrop-filter: blur(10px) ;
-    -webkit-backdrop-filter: blur(20px) saturate(180%);
-    background-color: rgba(253, 252, 252, 0.123);
-    border-radius: 12px;
-
- 
+.bg_description {
+  backdrop-filter: blur(10px);
+  -webkit-backdrop-filter: blur(20px) saturate(180%);
+  background-color: rgba(253, 252, 252, 0.123);
+  border-radius: 12px;
 }
 
-
-@import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@700&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@700&display=swap");
 </style>
--- src/views/Navigation/Navigation.vue
+++ src/views/Navigation/Navigation.vue
@@ -18,14 +18,14 @@
         <button
           data-collapse-toggle="navbar-dropdown"
           type="button"
-          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden  focus:outline-none focus:ring-2 focus:ring-gray-200 "
           aria-controls="navbar-dropdown"
           aria-expanded="false"
         >
           <span class="sr-only">Open main menu</span>
           <svg
             class="w-5 h-5"
-            aria-hidden="true"
+       
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 17 14"
@@ -38,6 +38,7 @@
               d="M1 1h15M1 7h15M1 13h15"
             />
           </svg>
+
         </button>
         <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
           <ul
@@ -153,6 +154,10 @@
 </template>
 
 <script setup>
+import {ref} from 'vue';
+
+const currentColor = 'rgb(255,255,255)'
+
 
 import Dropdown from 'primevue/dropdown';
 
