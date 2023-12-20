1ebcd53f8b06a894747f0ad701ac8ce4667a34ca
ozodsilver <ozodsilver@gmail.com>
2023-12-20 19:59:26 +0500

    language added

---
+++ src/Languages/index.js
@@ -0,0 +1,20 @@
+import { createI18n } from 'vue-i18n'
+
+import ru from '../Languages/ru.json'
+import uz from '../Languages/uz.json'
+import en from '../Languages/en.json'
+
+const languages = {
+  en,
+  ru,
+  uz
+}
+
+const i18n = createI18n({
+  legacy: false,
+  locale: 'ru',
+  fallbackLocale: 'uz',
+  messages: languages
+})
+
+export default i18n;
--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+
+
 import App from './App.vue'
 import router from './router'
+import i18n from './Languages/index.js'
 
 // import 'primeicons/primeicons.css'
 
@@ -21,6 +24,7 @@ import Vue3Marquee from 'vue3-marquee'
 import FocusTrap from 'primevue/focustrap';
 
 
+
 const app = createApp(App)
 
 
@@ -30,6 +34,7 @@ app.directive('ripple', Ripple);
 
 
 app.use(createPinia())
+app.use(i18n)
 app.use(Vue3Marquee)
 app.use(PrimeVue, { ripple: true });
 app.use(router)
