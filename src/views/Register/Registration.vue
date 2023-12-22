77fced73207f192a1429ea252b872880d38e266a
ozodsilver <ozodsilver@gmail.com>
2023-12-22 23:43:26 +0500

    language added

--- src/views/Register/Registration.vue
+++ src/views/Register/Registration.vue
@@ -14,8 +14,8 @@
             >Email</label
           >
           <input
+          v-model="email"
             type="email"
-            id="email"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="siyf@gmail.com"
             required
@@ -23,20 +23,19 @@
         </div>
         <div class="mb-5">
           <label
-            for="password"
             class="block mb-2 text-sm font-medium text-white dark:text-white"
             >Tel</label
           >
           <input
+          v-model="phone"
             type="tel"
-            id="password"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             required
             placeholder="+998 99 999 99 99"
           />
 
           <div class="card flex justify-content-center">
-        <Textarea v-model="value" class="w-full rounded-lg mt-6 text-sm h-[130px]" placeholder="Sizni qiziqtirgan savolni qoldiring" />
+        <Textarea v-model="textAreaValue" class="w-full rounded-lg mt-6 text-sm h-[130px]" placeholder="Sizni qiziqtirgan savolni qoldiring" />
     </div>
         </div>
 
@@ -57,7 +56,16 @@
 import { ref } from 'vue';
 import Textarea from 'primevue/textarea';
 
-const value = ref('');
+const email = ref('')
+const  phone = ref('')
+const textAreaValue = ref('')
+
+const data = {
+email:'email@gmail.com',
+phone:'998898777',
+feedback:'fuck you'
+}
+
 </script>
 
 <style lang="scss" scoped>
